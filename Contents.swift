d18a08527ef9c69b2caa4332358776c541221c8f
ChasBoss <cboss@e112-m-24.mchenry.edu>
2015-09-01 17:26:52 -0500

    Constants_And_VariablesChasB
    Week3 homework.

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numberLowerThanTen:Int = 9
+var numberHigherThanTen:Double = 11
+var rootBeer:Float = 49.00
+
+let numberYouShallNotChange1 = 1
+let numberYourShallNotChange2:Float = 4.11
+let ensuredFloat = numberYourShallNotChange2
+let dontTouchMyRootBeer = 485.456546549879879878946584987984
+
+let picolo:Int = 400
+let goku:Float = 9000.50
+let frieza:Double = 1000000
+
+let someFloat = 4
+someFloat = 100
+//The value stays at 4 and does not change.
+
+println(rootBeer)
+
+
+
+
